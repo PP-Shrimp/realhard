e3bdc3d32b3a203fe3c322af1af5c2ddd8b94fd8
v_haojianlong <v_haojianlong@baidu.com>
2018-12-29 16:55:48 +0800

    广告

--- src/components/adversiment/addAdversiment.vue
+++ src/components/adversiment/addAdversiment.vue
@@ -1,74 +1,55 @@
 <template>
   <div>
-    <Button @click="value3 = true" type="primary">新建</Button>
-    <Drawer title="Create" v-model="value3" width="720" :mask-closable="false" :styles="styles">
-      <Form :model="formData">
+    <Drawer title="新建代理商" v-model="value3" width="720" :mask-closable="false" :styles="styles">
+      <Form ref="formData" :model="formData" :rules="checkFormData">
         <Row :gutter="32">
           <Col span="12">
-            <FormItem label="Name" label-position="top">
-              <Input v-model="formData.name" placeholder="please enter user name"/>
+            <FormItem label="代理商名称" label-position="top" prop="name">
+              <Input v-model="formData.name" placeholder="请输入代理商名称" :readonly="status=='view'"/>
             </FormItem>
           </Col>
-          <Col span="12">
-            <FormItem label="Url" label-position="top">
-              <Input v-model="formData.url" placeholder="please enter url">
-                <span slot="prepend">http://</span>
-                <span slot="append">.com</span>
-              </Input>
+          <Col span="12" v-if="status!='add'">
+            <FormItem label="代理商编号" label-position="top" prop="num">
+              <Input v-model="formData.num" placeholder="请输入代理商编号" readonly />
             </FormItem>
           </Col>
         </Row>
-        <Row :gutter="32">
+        <Row :gutter="32">  
           <Col span="12">
-            <FormItem label="Owner" label-position="top">
-              <Select v-model="formData.owner" placeholder="please select an owner">
-                <Option value="jobs">Steven Paul Jobs</Option>
-                <Option value="ive">Sir Jonathan Paul Ive</Option>
+            <FormItem label="联系人" label-position="top" prop="person">
+              <Input v-model="formData.person" placeholder="请输入联系人" :readonly="status=='view'"/>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="联系电话" label-position="top" prop="phone">
+              <Input v-model="formData.phone" placeholder="请输入联系电话" :readonly="status=='view'"/>
+            </FormItem>
+          </Col>
+
+          <Col span="12">
+            <FormItem label="代理状态" label-position="top" prop="status">
+              <Select v-model="formData.status" placeholder="请选择代理状态" :disabled="status=='view'">
+                <Option value="0">代理中</Option>
+                <Option value="1">已注销</Option>
               </Select>
             </FormItem>
           </Col>
           <Col span="12">
-            <FormItem label="Type" label-position="top">
-              <Select v-model="formData.type" placeholder="please choose the type">
-                <Option value="private">Private</Option>
-                <Option value="public">Public</Option>
-              </Select>
-            </FormItem>
+          <FormItem label="代理时间" label-position="top" prop="date" >
+            <DatePicker :disabled="status=='view'"
+              v-model="formData.date"
+              type="daterange"
+              placeholder="请选择代理时间"
+              style="display: block"
+              placement="bottom-end"
+            ></DatePicker>
+          </FormItem>
           </Col>
         </Row>
-        <Row :gutter="32">
-          <Col span="12">
-            <FormItem label="Approver" label-position="top">
-              <Select v-model="formData.approver" placeholder="please choose the approver">
-                <Option value="jobs">Steven Paul Jobs</Option>
-                <Option value="ive">Sir Jonathan Paul Ive</Option>
-              </Select>
-            </FormItem>
-          </Col>
-          <Col span="12">
-            <FormItem label="DateTime" label-position="top">
-              <DatePicker
-                v-model="formData.date"
-                type="daterange"
-                placeholder="please select the date"
-                style="display: block"
-                placement="bottom-end"
-              ></DatePicker>
-            </FormItem>
-          </Col>
-        </Row>
-        <FormItem label="Description" label-position="top">
-          <Input
-            type="textarea"
-            v-model="formData.desc"
-            :rows="4"
-            placeholder="please enter the description"
-          />
-        </FormItem>
       </Form>
       <div class="demo-drawer-footer">
-        <Button style="margin-right: 8px" @click="value3 = false">Cancel</Button>
-        <Button type="primary" @click="value3 = false">Submit</Button>
+        <Button style="margin-right: 8px" @click="cancel('formData')">取消</Button>
+        <Button type="primary" @click="handleSubmit('formData')" v-if="status!='view'">保存</Button>
       </div>
     </Drawer>
   </div>
@@ -77,7 +58,29 @@
 export default {
   name: "addAdversiment",
   data() {
+    const checkName = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入代理商名称'));
+      }
+    };
+    const checkPerson = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入联系人'));
+      }
+    };
+    const checkPhone = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入联系电话'));
+      }
+    };
+    const checkDate = (rule, value, callback) => {
+      console.log(value);
+      if (value[0] === '') {
+        callback(new Error('请输入代理时间'));
+      }
+    };
     return {
+      name:'新建',
       value3: false,
       styles: {
         height: "calc(100% - 55px)",
@@ -85,16 +88,53 @@ export default {
         paddingBottom: "53px",
         position: "static"
       },
-      formData: {
-        name: "",
-        url: "",
-        owner: "",
-        type: "",
-        approver: "",
-        date: "",
-        desc: ""
+      checkFormData:{
+        name:[
+          { validator: checkName, trigger: 'blur' }
+        ], // 名称
+        person: [
+          { validator: checkPerson, trigger: 'blur' }
+        ],
+        phone: [
+          { validator: checkPhone, trigger: 'blur' }
+        ],
+        date: [
+          { validator: checkDate, trigger: 'blur' }
+        ]
       }
     };
+  },
+  props: {
+    formData: {
+      type: Object
+    },
+    status: {
+      type: String
+    }
+  },
+  methods: {
+    DrawerToShow() {
+      this.value3 = true;
+    },
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success('Success!')
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
+    },
+    cancel (name) {
+      this.value3 = false;
+      // this.$refs[name].resetFields();
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+
   }
 };
 </script>
--- src/view/adv/adv-pos-manage.vue
+++ src/view/adv/adv-pos-manage.vue
@@ -4,58 +4,192 @@
 <template>
   <div>
     <Row>
-      <addAdversiment></addAdversiment>
+      <Button @click="show('','add')" type="primary">新建</Button>
+      <Button @click="queryShow" type="primary" style="margin-left: 10px;">过滤
+        <Icon type="ios-arrow-down"></Icon>
+      </Button>
+      <addAdveriment :formData="curRow" :status="status" ref="Drawer"></addAdveriment>
     </Row>
-
+    <Row v-if="queryFlag" style="padding:20px 20px 0;">
+      <Form :model="queryData" :label-width="80">
+        <Col span="8">
+          <FormItem label="代理商名称" label-position="top" prop="name">
+            <Input v-model="queryData.name" placeholder="请输入代理商名称"/>
+          </FormItem>
+        </Col>
+        <Col span="8">
+          <FormItem label="代理状态" label-position="top" prop="status">
+            <Select v-model="queryData.status" placeholder="请选择代理状态">
+              <Option value="0">代理中</Option>
+              <Option value="1">已注销</Option>
+            </Select>
+          </FormItem>
+        </Col>
+        <Col span="8">
+          <FormItem label="代理时间" label-position="top" prop="date">
+            <DatePicker
+              v-model="queryData.date"
+              type="daterange"
+              placeholder="请选择代理时间"
+              style="display: block"
+              placement="bottom-end"
+            ></DatePicker>
+          </FormItem>
+        </Col>
+        <Col span="24">
+          <FormItem>
+              <Button type="primary">过滤</Button>
+          </FormItem>
+        </Col>
+      </Form>
+    </Row>
     <Row class="margin-top-10">
-      <Table :columns="tableTitle" :data="tableData"></Table>
+      <Table :columns="tableTitle" :data="tableData" stripe></Table>
+      <div style="margin: 10px;overflow: hidden">
+        <div style="float: right;">
+          <Page :total="total" :current="pageNo" @on-change="changePage" show-total></Page>
+        </div>
+      </div>
     </Row>
   </div>
 </template>
 <script>
-import addAdversiment from "@/components/adversiment/addAdversiment.vue";
+import addAdveriment from "@/components/adversiment/addAdversiment.vue";
 export default {
   name: "adv-pos-manage",
   components: {
-    addAdversiment
+    addAdveriment
   },
   data() {
     return {
-      exportLoading: false,
+      curRow: {
+        name: "", //名称
+        num: "", //编号
+        person: "", //联系人
+        phone: "", //联系电话
+        status: "0", //代理状态
+        date: ['','']//时间
+      },
+      status: "",
+      total: 50, //总记录条数
+      count: 5, //总页数
+      pageNo: 1, //当前页
+      queryFlag: false,
+      queryData: {
+        name:'',
+        status:'0',
+        date:''
+      },
       tableTitle: [
         {
-          title: "四级分类",
-          key: "category1"
+          title: "代理商编号",
+          key: "num"
         },
         {
-          title: "二级分类",
-          key: "category2"
+          title: "代理商名称",
+          key: "name"
         },
         {
-          title: "三级分类",
-          key: "category3"
+          title: "联系人",
+          key: "person"
+        },
+        {
+          title: "联系电话",
+          key: "phone"
+        },
+        {
+          title: "代理状态",
+          key: "status"
+        },
+        {
+          title: "代理时间",
+          key: "date"
+        },
+        {
+          title: "创建时间",
+          key: "category7"
+        },
+        {
+          title: "操作",
+          key: "action",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "text",
+                    size: "small"
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.row, "view");
+                    }
+                  }
+                },
+                "查看"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "text",
+                    size: "small"
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.row, "edit");
+                    }
+                  }
+                },
+                "编辑"
+              )
+            ]);
+          }
         }
       ],
       tableData: [
         {
-          category1: 555,
-          category2: 2,
-          category3: 3
+          num: 156312,
+          name: "云南腾云旅行社",
+          person: "舒展",
+          phone: "13596351234",
+          status: "代理中",
+          date: "2018-12-12 09:25",
+          category7: "2018-12-12 09:25"
         },
         {
-          category1: 4,
-          category2: 5,
-          category3: 6
-        },
-        {
-          category1: 7,
-          category2: 8,
-          category3: 9
+          num: 156512,
+          name: "云南腾云旅行社1",
+          person: "舒1展",
+          phone: "13596351234",
+          status: "代理中",
+          date: "2018-12-13 09:25",
+          category7: "2018-12-12 09:25"
         }
       ]
     };
   },
-  methods: {},
+  methods: {
+    // row:当前行数据
+    // view:编辑 or 查看
+    show(row, view) {
+      this.curRow = row || {
+        name: "", //名称
+        num: "", //编号
+        person: "", //联系人
+        phone: "", //联系电话
+        status: "0", //代理状态
+        date: ['',''] //时间
+      };
+      this.status = view;
+      this.$refs.Drawer.DrawerToShow();
+    },
+    changePage() {},
+    queryShow() {
+      this.queryFlag = !this.queryFlag;
+    }
+  },
   created() {},
   mounted() {}
 };
